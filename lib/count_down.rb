aab4e1e4a8310279594df6a2ea48e11afa120789
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-09 22:35:39 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,2 @@
 # Write your code here
 
-magic_exit_number = 7
-count = 10
-while count < 10 do
-  break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!" # Work
-  count = count + 1
-end
